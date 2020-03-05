e63bda6810148c9f5369bd0a4161cc9b4445a49b
Krunal Dodiya <krunaldodiya@Krunals-iMac.local>
2020-03-05 21:35:13 +0530

    added otp

--- src/screens/EditProfile.tsx
+++ src/screens/EditProfile.tsx
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {SafeAreaView, StatusBar, Text, View} from 'react-native';
+import {TextInputMask} from 'react-native-masked-text';
 
 function EditProfile() {
+  const [dob, setDob] = useState('27/06/1987');
+
   return (
     <>
       <StatusBar barStyle="dark-content" backgroundColor="#fff" />
 
       <SafeAreaView style={{flex: 1}}>
         <View style={{flex: 1, padding: 10}}>
-          <Text>EditProfile</Text>
+          <TextInputMask
+            style={{
+              backgroundColor: 'white',
+              paddingVertical: 10,
+              paddingLeft: 20,
+              borderRadius: 50,
+              elevation: 5,
+            }}
+            placeholder="Date of Birth (DD/MM/YYYY)"
+            type={'datetime'}
+            options={{format: 'DD/MM/YYYY'}}
+            value={dob}
+            onChangeText={text => setDob(text)}
+          />
         </View>
       </SafeAreaView>
     </>
